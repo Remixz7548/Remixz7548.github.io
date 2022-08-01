7f26b89c22ff310c9eae8436e484bedfe1783efa
Remixz7548 <74849956+Remixz7548@users.noreply.github.com>
2022-08-01 10:47:33 -0500

    nuevo intento

--- correo.php
+++ correo.php
@@ -1,5 +1,15 @@
-<?php 
-    $destinatario = 'parachichon75@gmail.com';//correo a envia el formulario
+<?php
+if($_SERVER['REQUEST_METHOD'] != 'POST' ){
+    header("Location: index.html" );
+}
+
+/*
+if( ! isset( $_POST['nombre'] ) ){
+    header("Location: index.html" );
+}
+*/
+
+
     $nombre = $_POST['name'];
     $email = $_POST['email'];
     $numero = $_POST['number'];
@@ -7,10 +17,34 @@
     $sistema = $_POST['sistema'];
     $mensaje = $_POST['mensaje'];
 
-    $header = "Enviado desde la pagina de Grupo Contsulta";
-    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+if( empty(trim($nombre)) ) $nombre = 'anonimo';
 
-    mail($destinatario, 'prueba', 'mensaje de prueba', 'prueba');
-    echo "<script>alert('Correo Enviado Exitosamente')</script>";
-    echo "<script>setTimeout(\"location.href='index.html'\",500)</script>";
-?>
+$body = <<<HTML
+    <h1>Contacto desde la web</h1>
+    <p>De: $nombre / $email / $numero</p>
+    <h2>Asunto</h2>
+    $asunto
+    <h2>Sistema</h2>
+    $sistema
+    <h2>Mensaje</h2>
+    $mensaje
+HTML;
+
+//sintaxis de los emails email@algo.com || 
+// nombre <email@algo.com>
+
+$headers = "MIME-Version: 1.0 \r\n";
+$headers.= "Content-type: text/html; charset=utf-8 \r\n";
+$headers.= "From: $nombre <$email> \r\n";
+$headers.= "To: Sitio web <parachichon75@gmail.com> \r\n";
+// $headers.= "Cc: copia@email.com \r\n";
+// $headers.= "Bcc: copia-oculta@email.com \r\n";
+
+
+//REMITENTE (NOMBRE/APELLIDO - EMAIL)
+//ASUNTO 
+//CUERPO 
+$rta = mail('parachichon75@gmail.com', "Mensaje web: $asunto", $body, $headers );
+//var_dump($rta);
+
+header("Location: index.html" );
